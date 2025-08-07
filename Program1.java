1ec79936794599c75724b5d7f2af848fe5618445
shivamchauhan121221 <135223274+shivamchauhan121221@users.noreply.github.com>
2025-08-07 18:39:20 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,8 @@
+class Program1{
+    public static void main(String[] args) {
+        int[] arr = {14,13,15,12,10,18};
+        for(int i=0; i<arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
